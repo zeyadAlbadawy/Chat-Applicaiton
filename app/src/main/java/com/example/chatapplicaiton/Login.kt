a406754003f5a55ecc6a7c71b5e4806b8b1e25e2
zeyad albadawy <zeyadalbadawyamm@gmail.com>
2024-07-28 21:35:02 +0300

    First Part Completed

---
+++ app/src/main/java/com/example/chatapplicaiton/Login.kt
@@ -0,0 +1,38 @@
+package com.example.chatapplicaiton
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Login : AppCompatActivity() {
+    private lateinit var editEmail : EditText
+    private lateinit var editPassword : EditText
+    private lateinit var btnLogin : Button
+    private lateinit var btnSignUp: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_login)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        editEmail = findViewById(R.id.edit_email)
+        editPassword = findViewById(R.id.edit_password)
+        btnLogin = findViewById(R.id.btnLogin)
+        btnSignUp = findViewById(R.id.btnSignUp)
+
+        btnSignUp.setOnClickListener{
+            val intent = Intent(this, SignUp::class.java)
+            startActivity(intent)
+        }
+    }
+}
