250465b7213e1d152a3ecb17324e3b224ab00738
Karim Gamal Elsayed <kareemabolisa@gmail.com>
2024-07-31 01:05:35 +0300

    auth commit2

--- app/src/main/java/com/example/chatapplicaiton/Auth.kt
+++ app/src/main/java/com/example/chatapplicaiton/Auth.kt
@@ -1,21 +1,25 @@
 package com.example.chatapplicaiton
 import android.util.Log
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthException
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 class Auth(val email : String,val pass :String) {
     private val auth = Firebase.auth
      companion object{
-         var error : String?=null
+         var error : AuthResult?=null
      }
-    fun Log_in() : String{
-        try {
-            auth.signInWithEmailAndPassword(email,pass)
-            Log.d("message","success")
-        }catch (e :Error){
-            Log.d("message","Fail")
-            error=e.toString()
+    fun Log_in() : String {
+
+        val credential = auth.signInWithEmailAndPassword(email, pass)
+        if (credential.isSuccessful) {
+            Log.d("message", "success")
+        }else{
+            error= credential.result
+            Log.d("message", error.toString())
         }
-        return error.toString()
+     return error.toString()
     }
 
 }
