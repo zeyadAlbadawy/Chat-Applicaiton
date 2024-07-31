27ea4e9d491b4320c74404c354a0e9ddc1680c2a
Karim Gamal Elsayed <kareemabolisa@gmail.com>
2024-07-31 07:05:48 +0300

    auth commit7

--- app/src/main/java/com/example/chatapplicaiton/Auth.kt
+++ app/src/main/java/com/example/chatapplicaiton/Auth.kt
@@ -1,4 +1,5 @@
 package com.example.chatapplicaiton
+import android.content.Intent
 import android.util.Log
 import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
@@ -6,37 +7,50 @@ import com.google.firebase.auth.FirebaseAuthException
 import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
-class Auth(val email : String,val pass :String) {
+class Auth(val email : String ?,val pass :String ?) {
     private val auth = Firebase.auth
     companion object{
         var errormap :MutableMap<String,String> ?=null
     }
 
     fun Log_in() {
+        if(email!=null&&pass !=null) {
+            auth.signInWithEmailAndPassword(email, pass).addOnCompleteListener {
 
-        auth.signInWithEmailAndPassword(email, pass).addOnCompleteListener {
+                if (it.isSuccessful) {
+                    Log.d("message", "success")
+                } else {
+                    try {
+                        throw it.exception ?: Exception("Firebase Exception")
+                    } catch (e: FirebaseAuthInvalidCredentialsException) {
+                        val error = e.message
+                        when (error) {
+                            "The email address is badly formatted." -> {
+                                errormap!!["invalid_email"] = "please enter valid email"
+                            }
 
-            if (it.isSuccessful) {
-                Log.d("message", "success")
-            }else {
-                try {
-                    throw it.exception ?: Exception("Firebase Exception")
-                } catch (e: FirebaseAuthInvalidCredentialsException) {
-                    val error = e.message
-                    when(error){
-                        "The email address is badly formatted." -> {
-                            errormap!!["invalid_email"]="please enter valid email"}
-                        "Authentication failed: There is no user record corresponding to this identifier. The user may have been deleted." -> {
-                            errormap!!["userNotfound"]="the user doesn't be founded"
+                            "Authentication failed: There is no user record corresponding to this identifier. The user may have been deleted." -> {
+                                errormap!!["userNotfound"] = "the user doesn't be founded"
+                            }
+
+                            "The password is invalid or the user does not have a password." -> {
+                                errormap!!["incorrect_password"] = "please enter the correct password"
+                            }
+
+
                         }
-                        "The password is invalid or the user does not have a password." -> {
-                            errormap!!["incorrect_password"]="please enter the correct password"
-                        }
-
                     }
                 }
             }
         }
 
+    }
+    fun signup(name : String){
+        auth.createUserWithEmailAndPassword(email!!,pass!!).addOnCompleteListener{
+            if(it.isSuccessful){
+                pass
+
+            }
+        }
     }
 }
