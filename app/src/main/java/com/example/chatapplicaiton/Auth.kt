7d59465ca7a41ef9333efe7c3361b755137071d7
Karim Gamal Elsayed <kareemabolisa@gmail.com>
2024-07-31 06:37:29 +0300

    Update Auth.kt

--- app/src/main/java/com/example/chatapplicaiton/Auth.kt
+++ app/src/main/java/com/example/chatapplicaiton/Auth.kt
@@ -3,19 +3,36 @@ import android.util.Log
 import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseAuthException
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 class Auth(val email : String,val pass :String) {
     private val auth = Firebase.auth
+    companion object{
+        var errormap :Map<String,String> ?=null
+    }
 
     fun Log_in() {
 
-        val credential = auth.signInWithEmailAndPassword(email, pass)
-        if (credential.isSuccessful) {
-            Log.d("message", "success")
-        }else{
-            Log.d("message", credential.result.toString())
-        }
-    }
+        auth.signInWithEmailAndPassword(email, pass).addOnCompleteListener {
 
+            if (it.isSuccessful) {
+                Log.d("message", "success")
+            }else {
+                try {
+                    throw it.exception ?: Exception("Firebase Exception")
+                } catch (e: FirebaseAuthInvalidCredentialsException) {
+                    val error = e.message
+                    when(error){
+                        "The email address is badly formatted." -> {
+                            errormap[""]="please enter invalid email"}
+                        "Authentication failed: There is no user record corresponding to this identifier. The user may have been deleted." -> {}
+                        "The password is invalid or the user does not have a password." -> {}
+
+                    }
+                }
+            }
+        }
+
+    }
 }
